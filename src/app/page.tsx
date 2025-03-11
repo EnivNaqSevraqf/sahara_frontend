20c0b4db271daa4dcedb17960ae2c929f2fadf3e
PhantomzBack <yugiarora@gmail.com>
2025-03-12 00:42:13 +0530

    Starting out with committing and stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,10 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Hi Harshpreet and Achyuth!{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
+              {/* src/app/page.tsx */}
+              Ravi here!
             </code>
             .
           </li>
@@ -39,7 +40,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Deploy it now guys!!
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
