a6605f816fbcc578bbef81732473af0c035c2e8a
PhantomzBack <yugiarora@gmail.com>
2025-03-14 23:19:28 +0530

    Remove Sidebar component from RootLayout and LoginLayout; add new login layout file

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import Sidebar from "@/components/Sidebar";
+
+
+export default function LoginLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+    {/* <Sidebar /> */}
+    <section>{children}</section> 
+    </>
+  );
+}
