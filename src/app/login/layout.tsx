2abeeffb3a4b700da8215849222d6809de1d89e9
PhantomzBack <yugiarora@gmail.com>
2025-04-06 22:15:44 +0530

    Build fix, downgraded to nextjs 14

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,8 +1,4 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import Sidebar from "@/components/Sidebar";
 
 
 export default function LoginLayout({
